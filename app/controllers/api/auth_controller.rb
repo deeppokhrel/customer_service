f8de54590dea08d74b4c7ad6d3d6509d14668ab5
Dip Pokhrel <dippokhrel@gmail.com>
2025-05-03 18:31:11 +0545

    Adding auth controller

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Api::AuthController < ApplicationController
+  before_action :validate_auth_token
+  skip_before_action :validate_token, only: :login
+
+  # POST /api/auth/login
+  def login
+    expiry_time = 5.hours.from_now.to_i
+    payload = { user: "api_client", exp: expiry_time }
+    secret_token = JWT.encode(payload, SECRET_KEY, "HS256")
+
+    render json: { token: secret_token, expiry: expiry_time }
+  end
+
+  private
+
+  def validate_auth_token
+    render json: { error: "Unauthorized" }, status: :unauthorized unless token && secret_valid?
+  end
+
+  def token
+    @token ||= request.headers["Authorization"]
+  end
+
+  def secret_valid?
+    [ ENV["SUPPORT_API_SECRET"], ENV["CRM_API_SECRET"], ENV["BILLING_API_SECRET"] ].any? { |secret| secret == token }
+  end
+end
