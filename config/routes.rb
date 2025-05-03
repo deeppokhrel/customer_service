7d75dd4ecd1f8bdece80abbcdb95164834d8eca4
Dip Pokhrel <dippokhrel@gmail.com>
2025-05-03 18:31:59 +0545

    adding api controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    post "auth/login", to: "auth#login"
+    get "auth/verify", to: "auth#verify"
+  end
 end
