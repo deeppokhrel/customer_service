893a0d57f9bf83ab5aa4ccf6e5ce0ae1038f3b9d
Dip Pokhrel <dippokhrel@gmail.com>
2025-05-03 18:36:42 +0545

    Adding Request Token Validator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :validate_token
+  SECRET_KEY = Rails.application.secret_key_base
+
+  private
+
+  def validate_token
+    begin
+      decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: "HS256" })
+      render json: { message: "Invalid Token" } unless decoded
+    rescue JWT::DecodeError => e
+      render json: { error: "Invalid token: #{e.message}" }, status: :unauthorized
+    end
+  end
+
+  def token
+    @token ||= @token ||= request.headers["Authorization"]
+  end
 end
